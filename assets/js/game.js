55da96716a62d9e6b6ba7f52c610ed5d0e8b707f
Chris Biertzer <c.biertzer@gmail.com>
2020-09-13 18:15:18 -0500

    final save

--- assets/js/game.js
+++ assets/js/game.js
@@ -64,8 +64,17 @@ var fight = function(enemyName) {
   };
 
 for(var i = 0; i < enemyNames.length; i++) {
+    if (playerHealth > 0){
+        window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+    }
+    else {
+        window.alert("You have lost your robot in battle! Game Over, Man!");
+        break;
+    }
     var pickedEnemyName = enemyNames[i];
     enemyHealth = 50;
+    //debugger;
+
     fight(pickedEnemyName);
 }
 
