b86e22b8acc69bd12e8da5ee30d739745b1fde84
Chris Biertzer <c.biertzer@gmail.com>
2020-09-16 18:29:11 -0500

    fixed skip fight bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,32 +1,42 @@
 var fightOrSkip = function() {
-  // ask user if they'd like to fight or skip using  function
+  // ask user if they'd liked to fight or run
   var promptFight = window.prompt('Would you like FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
 
-    // Conditional Recursive Function Call
-  if (promptFight === "" || promptFight === null) {
+  if (!promptFight) {
     window.alert("You need to provide a valid answer! Please try again.");
     return fightOrSkip();
   }
 
+  promptFight = promptFight.toLowerCase();
+
   // if user picks "skip" confirm and then stop the loop
-  if (promptFight === "skip" || promptFight === "SKIP") {
+  if (promptFight === "skip") {
     // confirm user wants to skip
     var confirmSkip = window.confirm("Are you sure you'd like to quit?");
 
     // if yes (true), leave fight
     if (confirmSkip) {
-      window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
-      // subtract money from playerMoney for skipping
-      playerInfo.playerMoney = playerInfo.money - 10;
-      shop();
+      window.alert(playerInfo.name + ' has decided to skip this fight. Goodbye!');
+      // subtract money from playerInfo.money for skipping
+      playerInfo.money = Math.max(0, playerInfo.money - 10);
+      
+      return true;
     }
-  }
+  }    
 }
 
 var fight = function(enemy) {
-  console.log(enemy);
+  var isPlayerTurn = true;
+
+  if (Math.random() > 0.5) {
+    isPlayerTurn = false;
+  }
+
   while (playerInfo.health > 0 && enemy.health > 0) {
-    fightOrSkip();
+    if  (fightOrSkip()) {
+      break;
+    }
+
     var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
 
     enemy.health = Math.max(0, enemy.health - damage);
