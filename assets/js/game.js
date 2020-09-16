1f43be852751ce6814d131e91750ebdda99f8706
Chris Biertzer <c.biertzer@gmail.com>
2020-09-16 13:43:48 -0500

    Upgrade Shop function to accept intergers

--- assets/js/game.js
+++ assets/js/game.js
@@ -131,25 +131,21 @@ var endGame = function() {
 
 var shop = function() {
   var shopOptionPrompt = window.prompt(
-    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter 1 for REFILL, 2 for UPGRADE, or 3 TO LEAVE."
   );
 
-  switch (shopOptionPrompt) {
-    case "refill":
-    case "Refill":
-    case "REFILL":    
+    shopOptionPrompt = parseInt(shopOptionPrompt);
+
+    switch (shopOptionPrompt) {
+    case 1:  
       playerInfo.refillHealth();
       break;
     
-    case "upgrade":
-    case "Upgrade":
-    case "UPGRADE":    
+    case 2:   
       playerInfo.upgradeAttack();
       break;
 
-    case "leave":
-    case "Leave":
-    case "LEAVE":    
+    case 3:    
       window.alert("Leaving the store.");
       break;
 
@@ -163,7 +159,6 @@ var shop = function() {
 
 var getPlayerName = function() {
   var name = "";
-    debugger;
     while (name === "" || name === null) {
       name = prompt("What is your robot's name?");
     }
